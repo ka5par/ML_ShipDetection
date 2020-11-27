5e67c45a5aed6d76c22542f1337244a63d43c81e
ka5par <karlkasparhaavel@gmail.com>
2020-11-27 12:30:44 +0200

    0.3 train add correct augs

--- module_training.py
+++ module_training.py
@@ -1,44 +1,43 @@
-# installation
-
-#pip install -q -U torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
-#pip install -q
-
-
 ## import
 
+# //TODO get rid of nonsense imports. 
 import os
 import copy
 import cv2
 import json
 import pycocotools
-import random
-import torch
-import torchvision
-import detectron2
+import random 
 
-import numpy as np
+import numpy as np 
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from pathlib import Path
 from collections import defaultdict
 
-
-import LossEvalHook
 assert torch.__version__.startswith("1.6")
+
 import detectron2
+import detectron2.data.transforms as T
+import detectron2.utils.comm as comm
+
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
-from detectron2.config import get_cfg
 from detectron2 import model_zoo
+from detectron2.engine import DefaultTrainer, DefaultPredictor
+from detectron2.config import get_cfg
+
+from detectron2.data import MetadataCatalog,DatasetMapper,build_detection_train_loader,build_detection_test_loader
+from detectron2.data import detection_utils as utils
 from detectron2.data.catalog import DatasetCatalog
-from detectron2.data.datasets import register_coco_instances
-from detectron2.data import MetadataCatalog
-from detectron2.engine import DefaultTrainer
-from detectron2.evaluation import COCOEvaluator
-from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.data.datasets import register_coco_instances 
+
+
+from detectron2.utils.visualizer import Visualizer
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+
+from detectron2.projects.deeplab import add_deeplab_config, build_lr_scheduler
 
 PATH = os.path.abspath(os.getcwd())
 
@@ -48,54 +47,74 @@ register_coco_instances("my_dataset_val",{},PATH + "/input/train_annotations.jso
 my_dataset_train_metadata = MetadataCatalog.get("my_dataset_train")
 dataset_dicts = DatasetCatalog.get("my_dataset_train")
 
+# //TODO Argumentize code. 
 ## configurations set up
-# //TODO add validation and data augmentations
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
 cfg.DATASETS.TRAIN = ("my_dataset_train",)
 cfg.DATASETS.TEST = ("my_dataset_val",)
-cfg.TEST.EVAL_PERIOD = 100
-cfg.DATALOADER.NUM_WORKERS = 4
-cfg.SOLVER.IMS_PER_BATCH = 4
-cfg.SOLVER.BASE_LR = 0.005
+#cfg.TEST.EVAL_PERIOD = 500 ## // TODO useful to set up eval_hook
+cfg.DATALOADER.NUM_WORKERS = 4 ## 4 per gpu
+cfg.SOLVER.IMS_PER_BATCH = 32 ## So many that it fulfills the gpu vram
+cfg.SOLVER.BASE_LR = 0.0025  # pick a good LR
 cfg.SOLVER.MAX_ITER = 500
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512 
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ship)
 
 ## configuration class
 
 # https://gist.github.com/ortegatron/c0dad15e49c2b74de8bb09a5615d9f6b
+def custom_mapper(dataset_dict):
+    # Implement a mapper, similar to the default DatasetMapper, but with your own customizations
+    dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+    image = utils.read_image(dataset_dict["file_name"], format="BGR")
+    transform_list = [T.RandomCrop("absolute",(256,256)), 
+                      T.RandomFlip(prob=0.5, horizontal=False, vertical=True),
+                      T.RandomFlip(prob=0.5, horizontal=True, vertical=False) 
+                     ]
+    image, transforms = T.apply_transform_gens(transform_list, image)
+    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+
+    annos = [
+        utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+        for obj in dataset_dict.pop("annotations")
+        if obj.get("iscrowd", 0) == 0
+    ]
+    instances = utils.annotations_to_instances(annos, image.shape[:2])
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dict
+
+
+from detectron2.engine import DefaultTrainer
+from detectron2.evaluation import COCOEvaluator
 class CocoTrainer(DefaultTrainer):
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
         if output_folder is None:
             os.makedirs("coco_eval", exist_ok=True)
             output_folder = "coco_eval"
-        return COCOEvaluator(dataset_name, cfg, True, output_folder)
+        return COCOEvaluator(dataset_name, cfg, False, output_folder)
+    
+    @classmethod
+    def build_train_loader(cls, cfg):
+        return build_detection_train_loader(cfg, mapper=custom_mapper)
+    
+    @classmethod
+    def build_lr_scheduler(cls, cfg, optimizer):
+        return build_lr_scheduler(cfg, optimizer)
 
-    def build_hooks(self):
-        hooks = super().build_hooks()
-        hooks.insert(-1, LossEvalHook.LossEvalHook(
-            cfg.TEST.EVAL_PERIOD,
-            self.model,
-            build_detection_test_loader(
-                self.cfg,
-                self.cfg.DATASETS.TEST[0],
-                DatasetMapper(self.cfg, True)
-            )
-        ))
-        return hooks
-
+# //TODO make tensorboard work wihth loss_hook. 
 ## Tensorflow board
 #%load_ext tensorboard
 #%tensorboard --logdir logs
 
 
+# //TODO make it check for checkpoints before ? 
 ## Start training.
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg)
-trainer.resume_or_load(resume=False)
-trainer.train() #Trainer will throw out non-annotated pictures.
+trainer = CocoTrainer(cfg) 
+trainer.resume_or_load(resume=True)
+trainer.train() #Trainer will throw out non-annotated pictures. 
 
 ## save the training.
 checkpointer = DetectionCheckpointer(trainer.model, save_dir="./")
